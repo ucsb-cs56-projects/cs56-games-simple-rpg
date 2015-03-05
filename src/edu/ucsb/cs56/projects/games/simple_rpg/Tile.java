ae51ebaf6379812ecf85ab75ebac48c034fa8975
Vivek Patel <vivek_patel@umail.ucsb.edu>
2015-03-05 11:08:26 -0800

    Changed Tile class so constructor creates a random (water or grass) tile

--- src/edu/ucsb/cs56/projects/games/simple_rpg/Tile.java
+++ src/edu/ucsb/cs56/projects/games/simple_rpg/Tile.java
@@ -1,35 +1,36 @@
 /**
- * 
+ *
  */
 package edu.ucsb.cs56.projects.games.simple_rpg;
 
-/**Class to represent all the tiles the game is made of
- * @author Alvin Tan
+/**
+ * Class to represent all the tiles the game is made of
  *
+ * @author Alvin Tan
  */
 public class Tile {
-	//instance variables
-	private int tileType;
-	
-	/**Default Constructor, makes a basic Tile
-	 * 
-	 */
-	public Tile(){
-		/*Tile Type:
+    //instance variables
+    private int tileType;
+
+    /**
+     * Default Constructor, makes a basic Tile
+     */
+    public Tile() {
+        /*Tile Type:
 		 * 0-grass
 		 * 1-water
 		 */
-		tileType=0;
-	}
-	
-	/**
-	 * @return the type of tile
-	 */
-	public int getType(){
-		return tileType;
-	}
-	
-	public void setType(int type){
-		tileType = type;
-	}
+        tileType = (int)(Math.random() * 2) + 1;
+    }
+
+    /**
+     * @return the type of tile
+     */
+    public int getType() {
+        return tileType;
+    }
+
+    public void setType(int type) {
+        tileType = type;
+    }
 }

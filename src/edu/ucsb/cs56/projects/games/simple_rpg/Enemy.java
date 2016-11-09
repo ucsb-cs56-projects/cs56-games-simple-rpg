4b03960de728d6faf78b8904c01ebae91f9bb514
Daniel Chojnacki <dwchojnacki@umail.ucsb.edu>
2016-11-08 22:23:30 -0800

    Added Entity class to cover all the various character classes

--- src/edu/ucsb/cs56/projects/games/simple_rpg/Enemy.java
+++ src/edu/ucsb/cs56/projects/games/simple_rpg/Enemy.java
@@ -6,14 +6,10 @@ package edu.ucsb.cs56.projects.games.simple_rpg;
 /**
  * Abstract class that every enemy shall inherit from
  *
- * @author Alvin Tan
+ * @author Alvin Tan, Daniel Chojnacki
  */
-public abstract class Enemy {
-    protected int hp, mp, lvl;
-    protected int maxHP, maxMP, minDMG, maxDMG;
-    protected int agi, str, sta, intel;
+public abstract class Enemy extends Entity{
     protected Reward r;
-    protected String name;
 
     /**
      * Constructor that initializes the name/type of the enemy represented
@@ -21,87 +17,8 @@ public abstract class Enemy {
      * @param s String to represent the name (ie. type) of enemy
      */
     protected Enemy(String s) {
+	super(s);
         r = new Reward();
-        name = s;
-    }
-
-    /**
-     * @return the hp
-     */
-    public int getHp() {
-        return hp;
-    }
-
-    /**
-     * @return the mp
-     */
-    public int getMp() {
-        return mp;
-    }
-
-    /**
-     * @return the lvl
-     */
-    public int getLvl() {
-        return lvl;
-    }
-
-    /**
-     * @return the maxHP
-     */
-    public int getMaxHP() {
-        return maxHP;
-    }
-
-    /**
-     * @return the maxMP
-     */
-    public int getMaxMP() {
-        return maxMP;
-    }
-
-    /**
-     * @return the agi
-     */
-    public int getAgi() {
-        return agi;
-    }
-
-    /**
-     * @return the str
-     */
-    public int getStr() {
-        return str;
-    }
-
-    /**
-     * @return the sta
-     */
-    public int getSta() {
-        return sta;
-    }
-
-    /**
-     * @return the intel
-     */
-    public int getIntel() {
-        return intel;
-    }
-
-    /**
-     * @return the name
-     */
-    public String getName() {
-        return name;
-    }
-
-    /**
-     * method to check if enemy is alive or dead.
-     *
-     * @return true if the Enemy is Dead, and false otherwise
-     */
-    public boolean isDead() {
-        return (hp <= 0);
     }
 
     /**
